3563b718f799716c458b0341230667e0732cd5ea
Tony Baldwin <tonyb@myownsite.me>
2015-07-02 21:58:31 +0200

    additions, design modifications

--- admin/index.php
+++ admin/index.php
@@ -3,7 +3,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link rel="stylesheet" type="text/css" title="Orig" href="../main.css" media="screen,projection" />
 <meta name="Author" content="Anthony Baldwin" />
-<title>12-press</title>
+<title>12Press Administration</title>
 </head>
 <body>
 <!-- 12press index --!>
@@ -51,7 +51,8 @@ if($act == "post") {
 ?>
 
 
-<h2>Database:</h2>
+<h4>Database:</h4>
+<p>Information is displayed here for you to verify, but must be managed in the admin/config.php file manually at this time.</p>
 <form action="index.php" method="post">
     <table>
         <tr>
@@ -61,10 +62,11 @@ echo "<td><input type=\"text\" name=\"dbname\" size=\"50\" value=\"$dbname\"></i
 echo "</tr><tr><td>db user:</td>";
 echo "<td><input type=\"text\" name=\"dbuser\" size=\"50\" value=\"$dbuser\"></input></td>";
 echo "</tr><tr><td>db password:</td>";
-echo "<td><input type=\"text\" name=\"dbpass\" size=\"50\" value=\"NOTFORYOU\"></textarea></td>";
+echo "<td><input type=\"text\" name=\"dbpass\" size=\"50\" value=\"NOT DISPLAYED FOR SECURITY REASONS\"></textarea></td>";
 echo "</tr><tr><td>db host:</td>";
 echo "<td><input type=\"text\" name=\"dbhost\" size=\"50\" value=\"$dbhost\"></textarea></td>";
-echo "</tr><tr><td>site url (no trailing slash):</td>";
+echo "</tr></tbody></table> <h4>Site Information</h4><table><tbody>";
+echo "<tr><td>Site URL:</td>";
 echo "<td><input type=\"text\" name=\"url\" size=\"50\" value=\"$url\"></textarea></td>";
 echo "</tr><tr><td>Site Title:</td>";
 echo "<td><input type=text name=sitetitle size=\"100\" value=\"$sitetitle\"></textarea></td>";
@@ -73,9 +75,9 @@ echo "<td><input type=text name=headtext size=\"100\" value=\"$headtext\"></text
 echo "</tr><tr><td>Footer Message:</td>";
 echo "<td><input type=text name=footext size=\"100\" value=\"$footext\"></textarea></td>";
 ?>
-</tr></table>
-    <input type="hidden" name="act" value="post"></input>
-    <input type="submit" name="submit" value="Submit"></input>
+</tr></table><hr />
+  <!--  <input type="hidden" name="act" value="post"></input>
+    <input type="submit" name="submit" value="Submit"></input> --!>
 </form>
 
 </div>
---
+++ what.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" title="Orig" href="main.css" media="screen,projection" />
+<meta name="Author" content="Anthony Baldwin" />
+<title>12Press - Greater New Haven Area NA Meetings and Events</title>
+</head>
+<body>
+<!-- transprocloud index --!>
+<?php
+include 'templates/header.php';
+
+include 'templates/navbar.php';
+
+ini_set('display_errors', "1");
+ini_set('error_reporting', E_ALL ^ E_NOTICE);
+?>
+
+<div id="main">
+<h1>What is the NA program?</h4>
+
+<p>NA is a nonprofit fellowship or society of men and women for
+whom drugs had become a major problem. We are recovering
+addicts who meet regularly to help each other stay clean. This
+is a program of complete abstinence from all drugs. There is
+only one requirement for membership, the desire to stop using.
+We suggest that you keep an open mind and give yourself a
+break. Our program is a set of principles written so simply that
+we can follow them in our daily lives. The most important thing
+about them is that they work.</p>
+<p>There are no strings attached to NA. We are not affiliated
+with any other organizations. We have no initiation fees or
+dues, no pledges to sign, no promises to make to anyone.
+We are not connected with any political, religious, or law
+enforcement groups, and are under no surveillance at any
+time. Anyone may join us regardless of age, race, sexual
+identity, creed, religion, or lack of religion.</p>
+<p>We are not interested in what or how much you used or who
+your connections were, what you have done in the past, how
+much or how little you have, but only in what you want to do
+about your problem and how we can help. The newcomer is
+the most important person at any meeting, because we can
+only keep what we have by giving it away. We have learned
+from our group experience that those who keep coming to our
+meetings regularly stay clean.</p>
+</div>
+
+<?php
+include 'templates/footer.php';
+?>
+</body>
+</html>
