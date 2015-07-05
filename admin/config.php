4ee62777d0882fec7474e08fa7065488cd2608ed
tony baldwin <tonybaldwin@users.noreply.github.com>
2015-07-05 08:49:25 -0400

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,11 +1,11 @@
 <?php
 // config file for 12press
 //
-$dbname = 'gnhaTest';
-$dbuser = 'webservant';
-$dbpass = 'cmF0ZWRhcmlzYXdpbmdkYXJiCg';
+$dbname = '$dbname';
+$dbuser = '$dbuser';
+$dbpass = '$dbpassword';
 $dbhost = 'localhost';
-$url = 'http://gnhana.liberame.org';
-$sitetitle = "GNHA NA Area Meetings &amp; Events";
-$footext = "Call the Connecticut Region of Narcotics Anonymous Statewide phone line. 1-800-627-3543 24 hours a day, 7 days a week!";
+$url = 'http://www.yoursite.org';
+$sitetitle = "some title here";
+$footext = "text for footer";
 $headtext = "Our message is HOPE, and the promise of FREEDOM!";
