f0f7962ac0d40ae5ca30f21fd42dc737729b0048
Tony Baldwin <tonyb@myownsite.me>
2015-07-06 04:05:42 +0200

    added readings, removed flyers and minutes, tweaks

---
+++ wdr.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<?php
+include 'include/meta.php';
+?>
+<link rel="stylesheet" type="text/css" title="Orig" href="main.css" media="screen,projection" />
+<meta name="Author" content="Anthony Baldwin" />
+<title>12Press - What is the NA Program?</title>
+</head>
+<body>
+<?php
+include 'include/header.php';
+
+include 'include/navbar.php';
+
+ini_set('display_errors', "1");
+ini_set('error_reporting', E_ALL ^ E_NOTICE);
+?>
+
+<div id="main">
+<div id="reading">
+
+<h1>We Do Recover</h1>
+
+<p>When at the end of the road we find that we can no longer function as a human being, either with or without drugs, we all face the same dilemma. What is there left to do? There seems to be this alternative: either go on as best we can to the bitter ends—jails, institutions or death—or find a new way to live. In years gone by, very few addicts ever had this last choice. Those who are addicted today are more fortunate. For the first time in man’s entire history, a simple way has been proving itself in the lives of many addicts. It is available to us all. This is a simple spiritual—not religious—program, known as Narcotics Anonymous.</p>
+
+<p>&copy; <a href="http://www.na.org">Narcotics Anonymous</a></p>
+</div>
+</div>
+
+<?php
+include 'include/footer.php';
+?>
+</body>
+</html>
