29d68800b9b8f1b4b7ba427c4b86cb3157b78273
Tony Baldwin <tonyb@myownsite.me>
2015-07-07 16:44:15 +0200

    added git io site links

--- 12press.php
+++ 12press.php
@@ -18,16 +18,16 @@ include 'include/navbar.php';
 
 <div id="main">
 <?php 
-echo "<p><a href=\"https://github.com/tonybaldwin/12press/wiki\" target=\"_new\"><img class=\"nlogo\" src=\"$siteurl/images/12pnavbutton.png\" alt=\"12press\"></a></p>";
+echo "<p><a href=\"http://tonybaldwin.github.io/12press/\" target=\"_new\"><img class=\"nlogo\" src=\"$siteurl/images/12pnavbutton.png\" alt=\"12press\"></a></p>";
 ?>
 
 <h1>ABOUT 12Press</h1>
 
 <p><strong>This 12Press site belongs to the Greater New Haven Area of the <a href="http://www.ctna.org">CT Region of Narcotics Anonymous</a>.</strong></p>
 
-<p><a href="https://github.com/tonybaldwin/12press/wiki">12Press</a> is an online meeting and event schedule platform for 12 step recovery fellowships and similar organizations.</p>
+<p><a href="http://tonybaldwin.github.io/12press/">12Press</a> is an online meeting and event schedule platform for 12 step recovery fellowships and similar organizations.</p>
 <?php
-echo "<p><a href=\"https://github.com/tonybaldwin/12press/wiki\">12Press</a> is <a href=\"http://www.gnu.org/philosophy/free-sw.html\" target=\"_new\">Free/Open Source Software</a>, licensed acording to the terms of the <a href=\"$siteurl/license.php\">GNU Affero General Public License, version 3 (or later)</a>.";
+echo "<p><a href=\"http://tonybaldwin.github.io/12press/\">12Press</a> is <a href=\"http://www.gnu.org/philosophy/free-sw.html\" target=\"_new\">Free/Open Source Software</a>, licensed acording to the terms of the <a href=\"$siteurl/license.php\">GNU Affero General Public License, version 3 (or later)</a>.";
 ?>
 <p>This means that you are free to download and install this software for your organization, view the code, modify the code, and share your modifications with others.</p>
 <p>If you'd like to be involved in further development of this platform, please feel free to clone <a href="https://github.com/tonybaldwin/12press" target="_new">the github repo</a> and dig into the code.
